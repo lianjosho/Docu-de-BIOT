915ab90c985d30232e739eba11dbc19c3b04826b
bristeFirmware <bristefirmware@gmail.com>
2023-08-18 15:33:16 -0300

    init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BIOT'
+copyright = '2023, BristeFirmware'
+author = 'BristeFirmware'
 
 release = '0.1'
 version = '0.1.0'
